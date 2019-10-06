e431d570e17b42eede12812f1976470686cd65bf
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-06 12:10:01 -0300

    Add files via upload

---
+++ 2746 - Virus.sql
@@ -0,0 +1,20 @@
+--- URI Online Judge SQL
+--- Copyright URI Online Judge
+--- www.urionlinejudge.com.br
+--- Problem 2746
+
+CREATE TABLE virus (
+  id INTEGER,
+  name VARCHAR(255)
+);
+
+
+INSERT INTO virus(id, name)
+VALUES 
+      (1, 'H1RT'),
+      (2, 'H7H1'),
+      (3, 'HUN8'),
+      (4, 'XH1HX'),
+      (5, 'XXXX');
+
+ SELECT REPLACE (name, 'H1', 'X') FROM virus;

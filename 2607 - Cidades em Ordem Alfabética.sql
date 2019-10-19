35104a101007f086d2c3ee107d47133e590529b6
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-19 18:56:48 -0300

    Update 2607 - Cidades em Ordem Alfabética.sql

--- 2607 - Cidades em Ordem Alfabética.sql
+++ 2607 - Cidades em Ordem Alfabética.sql
@@ -1,7 +1,9 @@
---- URI Online Judge SQL
---- Copyright URI Online Judge
---- www.urionlinejudge.com.br
---- Problem 2607
+/*
+Todos os meses a empresa pede um relatório das cidades que os fornecedores estão cadastrados. 
+Dessa vez não vai ser diferente, 
+faça uma consulta no Banco de Dados que retorne todas as cidades dos fornecedores, mas em ordem alfabética.
+OBS: Você não deve mostrar cidades repetidas.
+*/
 
 CREATE TABLE providers (
   id numeric PRIMARY KEY,

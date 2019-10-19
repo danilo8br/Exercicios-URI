42f57339e433c0216e9fdc0bbae42671391e162b
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-19 18:59:09 -0300

    Update 2613 - Filmes em Promoção.sql

--- 2613 - Filmes em Promoção.sql
+++ 2613 - Filmes em Promoção.sql
@@ -2,9 +2,10 @@
 Antigamente a locadora fez um evento em que vários filmes estavam em promoção, 
 queremos saber que filmes eram esses.
 Seu trabalho para nós ajudar é selecionar o ID e o nome dos filmes cujo preço for menor que 2.00.
-
+--------------
 Danilo Donato
 URI 2613
+--------------
 */
 
 Esquema

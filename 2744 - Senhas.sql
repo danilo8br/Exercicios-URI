06605f581780068d40cc3774af91650e67717e2c
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-19 19:05:48 -0300

    Update 2744 - Senhas.sql

--- 2744 - Senhas.sql
+++ 2744 - Senhas.sql
@@ -2,6 +2,7 @@
 Você foi contratado para dar consultoria a uma empresa. 
 Analisando o banco de dados você notou que as senhas gravadas dos usuários estão em formato de texto, 
 sendo que isso pode gerar uma falha de segurança, uma vez que elas não estão criptografadas.
+Por tanto você deve selecionar o id, a senha atual e a senha transformada em MD5 de cada usuário na tabela account.
 --------------
 Danilo Donato
 URI 2744

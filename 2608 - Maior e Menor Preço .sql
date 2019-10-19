6523cea3fd0c792e7c2fea1a7736ea3a45e13914
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-19 18:57:59 -0300

    Update 2608 - Maior e Menor Preço .sql

--- 2608 - Maior e Menor Preço .sql
+++ 2608 - Maior e Menor Preço .sql
@@ -1,3 +1,11 @@
+/*
+O setor financeiro da nossa empresa, está querendo saber os menores e maiores valores dos produtos, que vendemos.
+Para isso exiba somente o maior e o menor preço da tabela produtos.
+--------------
+Danilo Donato
+URI 2608
+-------------
+
 CREATE TABLE products (
   id numeric PRIMARY KEY,
   name varchar(50),

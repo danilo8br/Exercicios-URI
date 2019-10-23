a5ed7c82f438a8bfad23183a2e1a515daf9da9cd
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-22 22:02:59 -0300

    Update 2617 - Fornecedor Ajax SA.sql

--- 2617 - Fornecedor Ajax SA.sql
+++ 2617 - Fornecedor Ajax SA.sql
@@ -1,3 +1,13 @@
+/*O setor financeiro encontrou alguns problemas na entrega de um dos nossos fornecedores,
+a entrega dos produtos não condiz com a nota fiscal.
+Seu trabalho é exibir o nome dos produtos e o nome do fornecedor, para os produtos fornecidos pelo fornecedor ‘Ajax SA’.
+
+---------------
+Danilo Donato
+URI 2617
+---------------
+*/
+
 CREATE TABLE providers (
   id numeric PRIMARY KEY,
   name varchar(255),

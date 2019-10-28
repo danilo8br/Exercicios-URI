c40f4ce51a4957354886c8ef6ff8b9a6934ca239
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-28 13:32:53 -0300

    Update 2741 - Notas dos Alunos.sql

--- 2741 - Notas dos Alunos.sql
+++ 2741 - Notas dos Alunos.sql
@@ -1,4 +1,15 @@
+/*O semestre acabou na Universidade do Sul da Transilvânia. Todos os cursos tiveram suas notas fechadas,
+apenas a disciplina de Alquimia 104 não teve a lista de alunos aprovados.
+Portanto, você deverá mostrar a frase 'Approved: ' junto com o nome do aluno e a sua nota, 
+para os alunos que foram aprovados (grade ≥7).
 
+Lembre-se de ordenar a lista pela maior nota.
+
+--------------
+Danilo Donato
+URI 2741
+--------------
+*/
 CREATE TABLE students(
   id INTEGER PRIMARY KEY,
   name VARCHAR(255),

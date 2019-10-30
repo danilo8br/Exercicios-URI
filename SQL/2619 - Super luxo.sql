601d94f9b27332e768641a5e21eb2dcb2ddefc4c
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-30 16:21:56 -0300

    Update 2619 - Super luxo.sql

--- SQL/2619 - Super luxo.sql
+++ SQL/2619 - Super luxo.sql
@@ -1,4 +1,13 @@
-
+/*
+A nossa empresa está querendo fazer um novo contrato para o fornecimento de novos produtos superluxuosos,
+e para isso precisamos de alguns dados dos nossos produtos.
+Seu trabalho é exibir o nome dos produtos, nome dos fornecedores e o preço, 
+para os produtos cujo preço seja maior que 1000 e sua categoria seja ‘Super Luxury.
+---------------
+Danilo Donato
+URI 2619
+--------------
+*/
 
 CREATE TABLE providers (
   id numeric PRIMARY KEY,

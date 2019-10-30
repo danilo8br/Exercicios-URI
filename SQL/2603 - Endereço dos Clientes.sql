88cbddd0a210002ef8b1797c666a475da8da75fc
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-30 16:15:00 -0300

    Update 2603 - Endereço dos Clientes.sql

--- SQL/2603 - Endereço dos Clientes.sql
+++ SQL/2603 - Endereço dos Clientes.sql
@@ -1,7 +1,14 @@
---- URI Online Judge SQL
---- Copyright URI Online Judge
---- www.urionlinejudge.com.br
---- Problem 2603
+/*
+A empresa fará um evento comemorando os 20 anos de mercado,
+e para isso faremos uma grande comemoração na cidade de Porto Alegre. 
+Queremos também convidar todos os nossos clientes que estão cadastrados nessa cidade.
+O seu trabalho é nos passar os nomes e os endereços dos clientes que moram em 'Porto Alegre',
+para entregar os convites pessoalmente.
+------------------------
+Danilo Donato
+URI 2603
+-----------------------
+*/
 
 CREATE TABLE customers (
   id NUMERIC PRIMARY KEY,

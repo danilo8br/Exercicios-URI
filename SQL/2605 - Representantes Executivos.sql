8dc2220460e881f9d072f4ed859b27d929d15b14
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-30 16:16:08 -0300

    Update 2605 - Representantes Executivos.sql

--- SQL/2605 - Representantes Executivos.sql
+++ SQL/2605 - Representantes Executivos.sql
@@ -1,4 +1,13 @@
-
+/*
+O setor financeiro precisa de um relatório sobre os fornecedores dos produtos que vendemos. 
+Os relatórios contemplam todas as categorias, mas por algum motivo, 
+os fornecedores dos produtos cuja categoria é a executiva, não estão no relatório.
+Seu trabalho é retornar os nomes dos produtos e dos fornecedores cujo código da categoria é 6.
+---------------
+Danilo Donato
+URI 2605
+--------------
+*/
 
 CREATE TABLE categories (
   id numeric PRIMARY KEY,

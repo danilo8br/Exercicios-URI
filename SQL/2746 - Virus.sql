f1968e232ce04c07d6c667462d405c0183c80584
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-10-30 16:26:24 -0300

    Update 2746 - Virus.sql

--- SQL/2746 - Virus.sql
+++ SQL/2746 - Virus.sql
@@ -1,7 +1,15 @@
---- URI Online Judge SQL
---- Copyright URI Online Judge
---- www.urionlinejudge.com.br
---- Problem 2746
+/*
+Os vírus estão evoluindo, porém uma nova pesquisa tem provado que trocando algumas proteínas a vacina se torna imbatível. 
+A proteína H1 (Hemaglutinina) quando é substituída pela proteína X (Xenomorphina) tem efeitos interessantes 
+no combate de quase todas as doenças virais. 
+Alguns conspiracionistas dizem que após a descoberta dessa vacina algumas criaturas de 3 metros de altura foram vistas perto do laboratório,
+mas claro, isso é mentira.
+Portanto você deve substituir todo caractere 'H1' ( Hemaglutinina ) por 'X' ( Xenomorphina ).
+--------------
+Danilo Donato
+URI 2746
+-------------
+*/
 
 CREATE TABLE virus (
   id INTEGER,

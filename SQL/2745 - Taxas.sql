a029f6c8992283394c200cf09108b19bdd295e61
danilo8br <51414398+danilo8br@users.noreply.github.com>
2019-11-05 01:26:34 -0300

    Add files via upload

---
+++ SQL/2745 - Taxas.sql
@@ -0,0 +1,29 @@
+/*
+Você está indo para uma reunião no plano Internacional de Taxas Pessoais,
+sua proposta é: toda pessoa com renda acima de 3000 deve pagar uma taxa para o governo, essa taxa é 10% do que ela ganha.
+Portanto, mostre o nome da pessoa e o valor que ela deve pagar para o governo com a precisão de duas casas decimais.
+
+-------------
+Danilo Donato
+URI 2745
+-------------
+*/
+
+CREATE TABLE people (
+  id INTEGER PRIMARY KEY,
+  name CHARACTER VARYING (255),
+  salary NUMERIC
+);
+
+
+INSERT INTO people(id, name, salary)
+VALUES 
+      (1, 'James M. Tabarez', 883),
+      (2, 'Rafael T. Hendon', 4281),
+      (3, 'Linda J. Gardner', 4437),
+      (4, 'Nicholas J. Conn', 8011),
+      (5, 'Karol A. Morales', 2508),
+      (6, 'Lolita S. Graves', 8709);
+      
+
+SELECT name, ROUND(salary*10/100,2) AS tax FROM people WHERE salary > 3000;

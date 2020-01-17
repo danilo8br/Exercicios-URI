b580c69e0c559e226e6f26ad547fffd814cf5dba
danilo8br <51414398+danilo8br@users.noreply.github.com>
2020-01-17 00:08:46 -0300

    Update Soma Simples.py

--- Python/Soma Simples.py
+++ Python/Soma Simples.py
@@ -1,4 +1,9 @@
 """
+-------------
+Danilo Donato
+URI 1002
+-------------
+
 Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA.
 A seguir escrever o valor desta variável.
 
